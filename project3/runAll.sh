4d453c0ad1acee0da8b54958a176d53b313abfac
Valentina <vlin39@gmail.com>
2026-05-12 10:58:08 -0400

    modified

--- project3/runAll.sh
+++ project3/runAll.sh
@@ -8,7 +8,7 @@ if [ $# -ne 3 ]
 then
 	echo "Usage: `basename $0` <inputFolder/> <timeLimit> <logFile>"
 	echo "Description:"
-	echo -e "\t This script make calls to ./run.sh for all the files in the given inputFolder/"
+	echo -e "\t This script make calls to ./run0.sh for all the files in the given inputFolder/"
 	echo -e "\t Each run is subject to the given time limit in seconds."
 	echo -e "\t Last line of each run is appended to the given logFile."
 	echo -e "\t If a run fails, due to the time limit or other error, the file name is appended to the logFile with --'s as time and result. "
@@ -33,12 +33,26 @@ fi
 # Create the log file
 touch $logFile
 
+# Check for which timeout
+if command -v timeout >/dev/null 2>&1; then
+    TIMEOUT_CMD="timeout"
+elif command -v gtimeout >/dev/null 2>&1; then
+    TIMEOUT_CMD="gtimeout"
+else
+    TIMEOUT_CMD="perl_timeout"
+fi
+
 # Run on every file, get the last line, append to log file
 for f in $inputFolder*.*
 do
 	fullFileName=$(realpath "$f")
 	echo "Running $fullFileName"
-	timeout $timeLimit ./run.sh $fullFileName > output.tmp
+	if [[ "$TIMEOUT_CMD" == "perl_timeout" ]]; then
+		perl -e 'alarm shift; exec @ARGV' "$timeLimit" ./run.sh "$fullFileName" > output.tmp
+		
+	else
+		$TIMEOUT_CMD "$timeLimit" ./run.sh "$fullFileName" > output.tmp
+	fi
 	returnValue="$?"
 	if [[ "$returnValue" = 0 ]]; then 					# Run is successful
 		cat output.tmp | tail -1 >> $logFile				# Record the last line as solution
